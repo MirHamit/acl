c6167e2cdbcd2ad7f833c7a01b8ef5acdcd673ed
Hamid Musavi <hkratos7@gmail.com>
2022-03-29 10:50:42 +0430

    Fix For Laravel Authorization
    fix for using auth()->user()->can('permission-slug')

--- src/ACLServiceProvider.php
+++ src/ACLServiceProvider.php
@@ -57,7 +57,7 @@ class ACLServiceProvider extends ServiceProvider
             try {
                 Permission::get()->map(function ($permission) {
                     Gate::define($permission->slug, function ($user) use ($permission) {
-                        return $user->hasPermissionTo($permission);
+                        return $user->hasPermissionTo($permission->slug);
                     });
                 });
             } catch (Exception $e) {
